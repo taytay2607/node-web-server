ac1ae0235e6e75ebc9305fb2971e9a6b43b8458b
Naruepanat <cstaytay@hotmail.com>
2018-03-26 12:44:49 +0700

    Add project page

--- server.js
+++ server.js
@@ -51,6 +51,13 @@ app.get('/about',(req,res)=>{
     });
 });
 
+app.get('/project',(req,res)=>{
+    res.render('project.hbs',{
+        pageTittle:'About Project',
+    });
+});
+
+
 app.get('/bad',(req,res)=>{
     res.send({
         error:'bad request'
